6d1f003b98c4fe0370c220d12a44e91721153173
Damien <damien.huteau@hotmail.fr>
2015-02-20 12:22:08 +0100

    - suppression des fichiers .idea précédemment trackés.

--- app/Http/Controllers/VDMPostsController.php
+++ app/Http/Controllers/VDMPostsController.php
@@ -32,7 +32,7 @@ class VDMPostsController extends Controller {
             $this->_vdm_db = App::make('VDMMongoDB');
             $this->_vdm_posts = $this->_vdm_db->posts;
         }catch (\MongoException $e){
-            return $e->getMessage();
+            dd($e->getMessage());
         }
 
     }
